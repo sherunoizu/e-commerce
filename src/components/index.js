44c3a0366f5bb12accf2ac3dc5c43860232452e2
Nikita <nikitazapletin@gmail.com>
2023-01-27 16:52:36 +0300

    create product-card component and styled it and shop-page layout

--- src/components/index.js
+++ src/components/index.js
@@ -7,3 +7,5 @@ export { Directory } from "./directory/directory.component";
 export { SignUpForm } from "./sign-up-form/sign-up-form.component";
 
 export { SignInForm } from "./sign-in-form/sign-in-form.component";
+
+export { ProductCard } from "./product-card/product-card.component";
